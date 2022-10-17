678c61adb04fcf559612af996ada731223b101a7
Gabriel dos Santos OLiveira <gabrielsantosoliveira951@gmail.com>
2022-10-17 10:10:37 -0400

    feat: Connected with firebase/auth

--- src/screens/SingIn.tsx
+++ src/screens/SingIn.tsx
@@ -1,15 +1,43 @@
 import { useState } from "react";
 import { VStack, Heading, Icon, useTheme } from "native-base";
+import { Alert } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import auth from "@react-native-firebase/auth";
 import { Key, Envelope } from "phosphor-react-native";
 import Logo from "../assets/logo_primary.svg";
 import { Input } from "../components/Input";
+import { Text } from "native-base";
 import { Button } from "../components/Button";
 const SingIn = () => {
-  const [name, setName] = useState();
-  const [password, setPassword] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigation = useNavigation();
   const { colors } = useTheme();
   const handleSingIn = () => {
-    console.log(name, password);
+    if (!email || !password) {
+      return Alert.alert("Entrar", "Informe email e senha");
+    }
+    setIsLoading(true);
+    auth()
+      .signInWithEmailAndPassword(email, password)
+
+      .catch((error) => {
+        setIsLoading(false);
+        if (error.code === "auth/invalid-email") {
+          return Alert.alert("Entrar", "Email ou senha inválido");
+        }
+        if (error.code === "auth/wrong-password") {
+          return Alert.alert("Entrar", "Email ou senha inválido");
+        }
+        if (error.code === "auth/user-not-found") {
+          return Alert.alert("Entrar", "Email ou senha inválido");
+        }
+        return Alert.alert("Entrar", "Não foi possível entrar");
+      });
+  };
+  const handleSignUp = () => {
+    navigation.navigate("SignUp");
   };
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -21,16 +49,28 @@ const SingIn = () => {
         placeholder="Email"
         marginBottom={4}
         InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} />}
-        onChangeText={() => setName}
+        onChangeText={setEmail}
       />
       <Input
         mb={8}
         placeholder="Senha"
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} />}
         secureTextEntry
-        onChangeText={() => setPassword}
+        onChangeText={setPassword}
       />
-      <Button title="Entrar" w="full" onPress={handleSingIn} />
+
+      <Button
+        title="Entrar"
+        w="full"
+        onPress={handleSingIn}
+        isLoading={isLoading}
+      />
+      <Text alignSelf="flex-start" color="blueGray.300" mt={8}>
+        Não possui cadastro?{" "}
+        <Text color="green.400" onPress={handleSignUp}>
+          Entre por aqui
+        </Text>
+      </Text>
     </VStack>
   );
 };
