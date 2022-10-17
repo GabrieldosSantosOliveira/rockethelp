f6ee88e480eca6b7095e91e8acd85136acf5e751
Gabriel dos Santos OLiveira <gabrielsantosoliveira951@gmail.com>
2022-10-17 09:54:00 -0400

    style: Formatted with prettier

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,8 @@
-import { Button as ButtonNativeBase, IButtonProps, Heading } from "native-base";
+import {
+  Button as ButtonNativeBase,
+  IButtonProps,
+  Heading
+} from 'native-base';
 
 type Props = IButtonProps & {
   title: string;
@@ -12,7 +16,7 @@ const Button = ({ title, ...rest }: Props) => {
       fontSize="sm"
       rounded="sm"
       _pressed={{
-        bg: "green.500",
+        bg: 'green.500'
       }}
     >
       <Heading color="white" fontSize="sm">
