8576a2c8c4c61b5b9c0f73924d1750277ae9051c
Gabriel dos Santos OLiveira <gabrielsantosoliveira951@gmail.com>
2022-10-17 10:17:20 -0400

    style: Formatted with prettier

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,58 +1,92 @@
-import { useState } from "react";
-import { VStack, Heading, Icon, useTheme, Text } from "native-base";
-import { Alert } from "react-native";
-import auth from "@react-native-firebase/auth";
-import { Key, Envelope } from "phosphor-react-native";
-import Logo from "../assets/logo_primary.svg";
-import { Input } from "../components/Input";
-import { Button } from "../components/Button";
-import { useNavigation } from "@react-navigation/native";
+import auth from '@react-native-firebase/auth';
+import { useNavigation } from '@react-navigation/native';
+import {
+  VStack,
+  Heading,
+  Icon,
+  useTheme,
+  Text
+} from 'native-base';
+import { Key, Envelope } from 'phosphor-react-native';
+import { useState } from 'react';
+import { Alert } from 'react-native';
+
+import Logo from '../assets/logo_primary.svg';
+import { Button } from '../components/Button';
+import { Input } from '../components/Input';
+
 const SignUp = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const navigation = useNavigation();
   const { colors } = useTheme();
   const handleSingUp = () => {
     if (!email || !password) {
-      return Alert.alert("Cadastrar", "Informe email e senha");
+      return Alert.alert(
+        'Cadastrar',
+        'Informe email e senha'
+      );
     }
     if (password.length < 6) {
       return Alert.alert(
-        "Cadastrar",
-        "A senha deve ter pelo menos 6 caracteres"
+        'Cadastrar',
+        'A senha deve ter pelo menos 6 caracteres'
       );
     }
     setIsLoading(true);
     auth()
       .createUserWithEmailAndPassword(email, password)
-      .catch((error) => {
+      .catch(error => {
         setIsLoading(false);
-        if (error.code === "auth/invalid-email") {
-          return Alert.alert("Cadastrar", "Email já cadastrado");
+        if (error.code === 'auth/invalid-email') {
+          return Alert.alert(
+            'Cadastrar',
+            'Email já cadastrado'
+          );
         }
-        return Alert.alert("Cadastrar", "Não foi possível cadastrar ");
+        return Alert.alert(
+          'Cadastrar',
+          'Não foi possível cadastrar '
+        );
       });
   };
   const handleSingIn = () => {
-    navigation.navigate("SingIn");
+    navigation.navigate('SingIn');
   };
   return (
-    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+    <VStack
+      flex={1}
+      alignItems="center"
+      bg="gray.600"
+      px={8}
+      pt={24}
+    >
       <Logo />
-      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+      <Heading
+        color="gray.100"
+        fontSize="xl"
+        mt={20}
+        mb={6}
+      >
         Crie sua Conta
       </Heading>
       <Input
         placeholder="Email"
         marginBottom={4}
-        InputLeftElement={<Icon as={<Envelope color={colors.gray[300]} />} />}
+        InputLeftElement={
+          <Icon
+            as={<Envelope color={colors.gray[300]} />}
+          />
+        }
         onChangeText={setEmail}
       />
       <Input
         mb={8}
         placeholder="Senha"
-        InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} />}
+        InputLeftElement={
+          <Icon as={<Key color={colors.gray[300]} />} />
+        }
         secureTextEntry
         onChangeText={setPassword}
       />
@@ -62,8 +96,12 @@ const SignUp = () => {
         onPress={handleSingUp}
         isLoading={isLoading}
       />
-      <Text alignSelf="flex-start" color="blueGray.300" mt={8}>
-        Já possui cadastro?{" "}
+      <Text
+        alignSelf="flex-start"
+        color="blueGray.300"
+        mt={8}
+      >
+        Já possui cadastro?{' '}
         <Text color="green.400" onPress={handleSingIn}>
           Entre por aqui
         </Text>
